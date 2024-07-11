abbf71597ca8bf955b18f26a7baeef9045421960
Sayan Some <sayansome84@gmail.com>
2024-07-11 10:37:21 +0530

    above 18 function added

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const age = 24;
-const name = 'Sayan some';
+const name1= 'Sayan some';
+
+
+function sayName(){
+    console.log('My name is' + name1)
+
+}
+
+function isAbove(){
+    return age >= 18 ;
+}
